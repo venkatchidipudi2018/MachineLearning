b8a8b53bf08685763ddd23c5a9bd3b7a9432751b
venkat.chidipudi@gmail.com <venkat.chidipudi@gmail.com>
2018-08-11 00:57:11 +0530

    Added feature scaling technique

--- Data_Preprocessing_Python/data_preprocessing_template.py
+++ Data_Preprocessing_Python/data_preprocessing_template.py
@@ -2,14 +2,16 @@
 @author: venkatchidipudi
 """
 #importing the libraries
-import numpy as np  #Contains mathematical tools, to include any type of mathematical operations
-import matplotlib.pyplot as plt  #Helps to plot nice charts
+import numpy as np  #Contains the mathematical tools and includes any type of mathematical operations
+import matplotlib.pyplot as plt  #Helps to plot the nice charts
 import pandas as pd  #To import datasets and manage datasets
+
 
 #importing the datasets
 dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
+
 
 #Taking care of missing data
 from sklearn.preprocessing import Imputer
@@ -17,11 +19,24 @@ imputer = Imputer(missing_values='NaN', strategy="mean", axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+
 #Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features= [0])
 X = onehotencoder.fit_transform(X).toarray()
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(Y)
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+
+#Splitting the dataset into training dataset and test dataset
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
--- Data_Preprocessing_R/data_preprocessing_template.R
+++ Data_Preprocessing_R/data_preprocessing_template.R
@@ -1,6 +1,7 @@
 #importing dataset
 dataset = read.csv("Data.csv")
-View(dataset)
+#View(dataset)
+
 
 #Taking care of missing values
 dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
@@ -17,3 +18,17 @@ dataset$City = factor(dataset$City, levels = c('Bangalore', 'Hyderabad', 'Chenna
 
 dataset$Purchased = factor(dataset$Purchased, levels = c('Yes', 'No'),
                       labels = c(1, 0))
+
+
+#Splitting the dataset into training dataset and test dataset
+#install.packages('caTools') --> already installed
+library(caTools)
+set.seed(123)
+split = dataset.split(dataset$Purchased, SplitRatio = 0.8)
+training_dataset = subset(dataset, split == TRUE)
+test_dataset = subset(dataset, split == FALSE)
+
+
+#Feature Scaling
+training_dataset[,2:3] = scale(training_dataset[,2:3])
+test_dataset[,2:3] = scale(test_dataset[,2:3])
