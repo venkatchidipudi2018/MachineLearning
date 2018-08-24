3c57f3df3fd8c2e326fd7816b8c45e2cb1acd50d
venkat.chidipudi@gmail.com <venkat.chidipudi@gmail.com>
2018-08-24 20:42:34 +0530

    Added all the classification models into natural language processing

--- Natural_Language_Processing/Python/natural_language_processing.py
+++ Natural_Language_Processing/Python/natural_language_processing.py
@@ -37,9 +37,34 @@ Y = dataset.iloc[:, 1].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
 
-#Fitting Naive Bayes to the training set
+"""#Fitting Naive Bayes to the training set
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
+classifier.fit(X_train, Y_train)
+
+#Fitting into Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = "entropy", random_state = 0)
+classifier.fit(X_train, Y_train)
+
+#Fitting into K-NN 
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors  = 5, metric  = "minkowski", p = 2)
+classifier.fit(X_train, Y_train)
+
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = "rbf", random_state = 0 )
+classifier.fit(X_train, Y_train)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, Y_train)"""
+
+# Fitting Random Forest to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators  = 10, criterion  = "entropy", random_state = 0)
 classifier.fit(X_train, Y_train)
 
 #Predicting the Test set result
